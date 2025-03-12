0e778f40abdc2575628e9467619febb5d49022c1
Akram Abdeslem Chaima <akram@riseup.net>
2025-03-12 22:59:06 +0100

    update craftblue print

--- Craft Blueprint/dim.py
+++ Craft Blueprint/dim.py
@@ -1,8 +1,9 @@
 # SPDX-License-Identifier: BSD-2-Clause
-# SPDX-FileCopyrightText: 2024 Your Name <your.email@example.com>
+# SPDX-FileCopyrightText: 2025 Akram Abdeslem Chaima <akram@riseup.net>
 
 import info
-from Package.CMakePackageBase import *
+from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
 
 
 class subinfo(info.infoclass):
@@ -26,6 +27,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier2/kcolorscheme"] = None
         self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
+        self.runtimeDependencies["libs/qt6/qt5compat"] = None
         self.runtimeDependencies["qt-libs/poppler"] = None
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
@@ -35,18 +37,6 @@ class subinfo(info.infoclass):
             self.runtimeDependencies["kde/plasma/qqc2-breeze-style"] = None
 
 
-
 class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-    def createPackage(self):
-        # Set the executable name for your application
-        self.defines["executable"] = r"bin\DGestapp.exe"
-        # Add filters to exclude unnecessary executables
-        self.addExecutableFilter(r"(bin|libexec)/(?!(DGestapp|update-mime-database)).*")
-        # Ignore unnecessary packages
-        self.ignoredPackages.append("binary/mysql")
-        if not CraftCore.compiler.isLinux:
-            self.ignoredPackages.append("libs/dbus")
-        return super().createPackage()
